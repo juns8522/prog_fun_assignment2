821271e0a89120cb1e3c4fad0424cb0c048d21e9
June Sung Lee <juns8522@gmail.com>
2015-10-01 17:55:45 +1000

    Create Action.java

---
+++ Action.java
@@ -0,0 +1,10 @@
+public class Action
+{
+    public static final int NONE = -1;
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int UP = 2;
+    public static final int DOWN = 3;
+
+    private Action() {}
+}
